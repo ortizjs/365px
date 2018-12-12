9488d3046ad860b73dd1b4a2191f13f2fdb68954
Jonnatan Ortiz <ortizjs@uci.edu>
2018-12-12 11:56:37 -0800

    Edit entry file and add App and Root

--- frontend/365px.jsx
+++ frontend/365px.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
     const root = document.getElementById('root');
     // console.log("Hello");
-    const store = configureStore();
-    window.getState = store.getState;
-    window.dispatch = store.dispatch; // testing purposes
-    ReactDOM.render(<h1>Welcome to 365px!</h1>, root);
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch; // testing purposes
+    ReactDOM.render(<Root store={ store }/>, root);
 });
---
+++ frontend/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <header>
+            <h1>365px!</h1>
+        </header>
+        <switch>
+
+        </switch>
+    </div>
+
+);
+export default App;
