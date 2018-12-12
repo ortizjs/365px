3a8dca4eb4c7f2dd6f72b2861bd40b8cc1e70c2e
Jonnatan Ortiz <ortizjs@uci.edu>
2018-12-12 11:21:29 -0800

    Edit entry file and install package

--- frontend/365px.jsx
+++ frontend/365px.jsx
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     // console.log("Hello");
     const store = configureStore();
-    window.getStore = store.getState;
+    window.getState = store.getState;
     window.dispatch = store.dispatch; // testing purposes
     ReactDOM.render(<h1>Welcome to 365px!</h1>, root);
 });
