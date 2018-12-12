6c5a6e4576fe96045e8ba340bf7395278e6ef3bb
Jonnatan Ortiz <ortizjs@uci.edu>
2018-12-12 10:14:46 -0800

    Create users and entities reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -7,15 +7,15 @@ const _nullUser = Object.freeze({
     id: null
 });
 
-const sessionReducer = (state = _nullUser, action) => {
-    Object.freeze(state);
+const sessionReducer = (oldState = _nullUser, action) => {
+    Object.freeze(oldState);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
             return { id: action.currentUser.id };
         case LOGOUT_CURRENT_USER:
             return _nullUser;
         default:
-            return state;
+            return oldState;
     }
 };
 
