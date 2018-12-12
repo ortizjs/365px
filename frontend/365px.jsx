05bdb245de9424be638bb0c25353617b710b760e
Jonnatan Ortiz <ortizjs@uci.edu>
2018-12-12 11:05:28 -0800

    Add store and modify entry file

--- frontend/365px.jsx
+++ frontend/365px.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     // console.log("Hello");
+    const store = configureStore();
+    window.getStore = store.getState;
+    window.dispatch = store.dispatch; // testing purposes
     ReactDOM.render(<h1>Welcome to 365px!</h1>, root);
 });
