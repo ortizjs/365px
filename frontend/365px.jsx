d5c1cc7f05d84e307031b6cd95d301836d8fdf46
Jonnatan Ortiz <ortizjs@uci.edu>
2018-12-11 17:00:54 -0800

    First commit  initial setup

---
+++ frontend/365px.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    // console.log("Hello");
+    ReactDOM.render(<h1>Welcome to 365px!</h1>, root);
+});
