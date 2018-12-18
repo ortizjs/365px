32f05ec80b7eea8964a9a7ed5741a32d5fb783b9
Jonnatan Ortiz <ortizjs@uci.edu>
2018-12-17 20:19:44 -0800

    Update avatar photo

--- frontend/365px.jsx
+++ frontend/365px.jsx
@@ -23,9 +23,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store} />, root);
-
-    window.fetchPhotos = fetchPhotos;
-    window.fetchPhoto = fetchPhoto;
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch; // just for testing!
+    // window.fetchPhotos = fetchPhotos;
+    // window.fetchPhoto = fetchPhoto;
 });
 
 // let user1 = {
@@ -35,4 +36,9 @@ document.addEventListener("DOMContentLoaded", () => {
 //     lastName: "Halpert",
 //     password: "1234567"
 //     };
-    
+
+// let post2 = {
+//     title: "This is a test photo post",
+//     photographer_id: 14,
+//     photo_url: "/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--277c6270d219edebacdf72e9b0a8e48a472f15e3/el-salvador777-.jpg)--)"
+// };
