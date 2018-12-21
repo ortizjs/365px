bf8b72c3885b10753694087c920a094b57b5153f
Jonnatan Ortiz <ortizjs@uci.edu>
2018-12-21 10:36:43 -0800

    Add style to photo title

--- frontend/components/photos/photos_index_item.jsx
+++ frontend/components/photos/photos_index_item.jsx
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom';
 
 const PhotoIndexItem = props => (
     <div className="photos-index-item-div">
+        
+        <div className="photo-title">
             {props.title}
-                <img src={props.photo.photo_url} alt="" />   
+        </div>
+        <img src={props.photo.photo_url} alt="" />   
         {/* <Link to={`api/photos/${props.photo.id}`}>{props.post.title}</Link> */}
         {/* <Link to={`api/photos/${props.post.id}/edit`}>Edit {props.post.title}</Link> */}
         {/* <button onClick={props.editPhoto(props.photo.id)}>Edit</button> */}
