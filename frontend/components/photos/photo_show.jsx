16ad827dcf72721b5e64fb5a152d1ec86ae21bb9
Jonnatan Ortiz <jonnatano94@gmail.com>
2019-01-30 10:21:20 -0800

    Updat photo show info style

--- frontend/components/photos/photo_show.jsx
+++ frontend/components/photos/photo_show.jsx
@@ -18,26 +18,26 @@ class PhotoShow extends React.Component {
                 <div className="photo-show-main-div">
                     <div className="photo-show-content">
                         <div className="image-div">
-                            <img className="photo-show-image" src={this.props.photo.photo_url} alt="" />
+                            <img className="photo-show-image" src={this.props.photo.photo_url} alt="" height="50%" width="50%"/>
                         </div>
                         <div className="show-page-photo-info">
-                            <h2>
+                            <h2 className="photo-show-title">
                                 {this.props.photo.title}
                             </h2>
-                            <h3>
+                            <h3 className="photo-show-by">
                                 By: {this.props.photo.first_name} {this.props.photo.last_name}
                             </h3>
+                            <div className="show-return-button">
+                                <button onClick={() => this.props.history.push("/users")}> Close </button>
+                                {/* <button onClick={() => deletePhoto(photo.id).then(() => that.props.history.go(0))}> Delete </button> */}
+                                {/* <Link to="/users">Go Back</Link> */}
+                            </div>
                         </div>
-                        <div className="photo-by-div">
+                        {/* <div className="photo-by-div"> */}
                             {/* <h3>
                                 By: {this.props.photo.first_name} {this.props.photo.last_name}
                             </h3> */}
-                        </div>
-                        <div className="show-return-button">
-                            <button onClick={() => this.props.history.push("/users")}> Close </button>
-                            {/* <button onClick={() => deletePhoto(photo.id).then(() => that.props.history.go(0))}> Delete </button> */}
-                            {/* <Link to="/users">Go Back</Link> */}
-                        </div>
+                        {/* </div> */}
                     </div>    
                 </div>
             )
